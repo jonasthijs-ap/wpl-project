7b4b541421bb3145ebcfa324d9cf128a9b4ce6ff
jonasthijs-ap <jonas.thijs@student.ap.be>
2024-04-24 08:54:38 +0000

    Indexbestand 'server.ts' verder aangevuld met de nodige basis om Express te laten draaien + nog enkele extra packages geïnstalleerd

--- server.ts
+++ server.ts
@@ -1,4 +1,18 @@
-import * as express from "express";
-
-import * as interfaces from "./types/types.ts";
+import express from "express";
+import { MongoClient, ObjectId } from "mongodb";
+import * as Interfaces from "./types/types";
 import * as tempMockFunctions from "./functions/tempMock.ts";
+
+const app: express.Express = express();
+const client: MongoClient = new MongoClient();
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("port", 3000);
+
+app.listen(app.get("port"), async () => {
+    console.info(`Express listening to 'http://localhost:${app.get("port")}'`);
+});
