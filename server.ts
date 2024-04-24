f3527fd38868bcee53c3521f2231b3e1e2f91552
jonasthijs-ap <jonas.thijs@student.ap.be>
2024-04-24 09:12:01 +0000

    Commentaar toegevoegd aan server.ts als verduidelijking van de aparte onderdelen in het bestand/de applicatie

--- server.ts
+++ server.ts
@@ -1,18 +1,25 @@
+// Importeren van modules
 import express from "express";
 import { MongoClient, ObjectId } from "mongodb";
 import * as Interfaces from "./types/types";
 import * as tempMockFunctions from "./functions/tempMock.ts";
 
+// Aanmaken Express-applicatie en connectie met MongoDB maken
 const app: express.Express = express();
 const client: MongoClient = new MongoClient();
 
+// Map met statische bestanden definiëren (zoals foto's, video's, fonts, ...)
 app.use(express.static("public"));
-app.use(express.json());
+
+// Maakt POST-requests met JSON mogelijk
+app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+// Express-variabelen aanmaken zoals mogelijk maken van rendering van EJS en de applicatiepoort instellen
 app.set("view engine", "ejs");
 app.set("port", 3000);
 
+// Maakt het mogelijk om de Express-applicatie te laten draaien op de ingestelde poort
 app.listen(app.get("port"), async () => {
     console.info(`Express listening to 'http://localhost:${app.get("port")}'`);
 });
