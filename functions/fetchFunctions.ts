76e081820f6a46d92ed4d56deb7546f653d5dba2
Ellie65368 <ellie65368@gmail.com>
2024-04-25 09:34:43 +0000

    API aanmaken

--- functions/fetchFunctions.ts
+++ functions/fetchFunctions.ts
@@ -1,5 +1,5 @@
 app.get("/users",async (req,res) =>{
-    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let response = await fetch("https://rebrickable.com/api/v3/lego/minifigs");
     let data = await response.json();
     res.type("application/json");
     res.json(data);
