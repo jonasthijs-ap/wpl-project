4762a827e6a655a9b098811580a761b982c767eb
Ellie65368 <ellie65368@gmail.com>
2024-04-29 14:07:04 +0000

    interface aangemaakt

--- functions/fetchFunctions.ts
+++ functions/fetchFunctions.ts
@@ -1,7 +1,7 @@
 import { Minifig } from "../types/types";
 export async function getMinifigs(set_num: string): Promise<Minifig>{
     const response = await fetch(
-        `https://rebrickable.com/api/v3/swagger/?key=55564703db4246a9f134ad8465a30a48#!/lego/lego_minifigs_read`, {headers: {Authorization: `key ${process.env.API_KEY}`}}
+        `https://rebrickable.com/api/v3/swagger/lego/lego_minifigs_read`, {headers: {Authorization: `key ${process.env.API_KEY}`}}
     );
     console.log(response);
     return (await response.json()) as Minifig;
