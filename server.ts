66b3557864a5de4f06b78f390b982cd4c108e5f2
jonasthijs-ap <jonas.thijs@student.ap.be>
2024-04-29 17:42:41 +0000

    .env functionaliteit toegevoegd

--- server.ts
+++ server.ts
@@ -1,12 +1,20 @@
 // Importeren van modules
+import dotenv from "dotenv";
 import express from "express";
 import { MongoClient, ObjectId } from "mongodb";
 import * as Interfaces from "./types/types";
 import * as tempMockFunctions from "./functions/tempMock.ts";
 
+// .env-settings
+dotenv.config();
+
 // Aanmaken Express-applicatie en connectie met MongoDB maken
 const app: express.Express = express();
-const client: MongoClient = new MongoClient();
+const client: MongoClient = new MongoClient(process.env.MONGO_URI || "");
+
+// Omgevingsvariabelen en Express-variabelen
+app.set("view engine", "ejs");
+app.set("port", process.env.PORT);
 
 // Map met statische bestanden definiëren (zoals foto's, video's, fonts, ...)
 app.use(express.static("public"));
@@ -14,10 +22,6 @@ app.use(express.static("public"));
 // Maakt POST-requests met JSON mogelijk
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
-
-// Express-variabelen aanmaken zoals mogelijk maken van rendering van EJS en de applicatiepoort instellen
-app.set("view engine", "ejs");
-app.set("port", 3000);
 
 // Maakt het mogelijk om de Express-applicatie te laten draaien op de ingestelde poort
 app.listen(app.get("port"), async () => {
